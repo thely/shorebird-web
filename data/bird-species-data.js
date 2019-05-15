29882b5a5897c8490e4a2ab083a213a3852b2fdc
Becky Brown <bardbarienne@gmail.com>
2019-05-15 02:46:22 -0400

    adding some datasets

---
+++ data/bird-species-data.js
@@ -0,0 +1,572 @@
+var bird_species_data = {
+   "0": {
+      "name": "ACTMCL",
+      "common_name": "Spotted Sandpiper",
+      "scientific_name": "Actitis macularia",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+   },
+   "1": {
+      "name": "AGLPHN",
+      "common_name": "Red-Winged Blackbird",
+      "scientific_name": "Agelaius phoeniceus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+   },
+   "2": {
+      "name": "AMMMRT",
+      "common_name": "Seaside Sparrow",
+      "scientific_name": "Ammodramus maritimus",
+      "land_preference": [13,14,15]
+   },
+   "3": {
+      "name": "ANSCLY",
+      "common_name": "Nothern Shoveler",
+      "scientific_name": "Anas clypeata",
+      "land_preference": [13,14,15,16,17,18,0,-1]
+   },
+   "4": {
+      "name": "ANSDSC",
+      "common_name": "Blue-Winged Teal",
+      "scientific_name": "Anas discors",
+      "land_preference": [13,14,15,16,17,18,0,-1]
+   },
+   "5": {
+      "name": "ANSPLT",
+      "common_name": "Mallard",
+      "scientific_name": "Anas platyrhynchos",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "6": {
+      "name": "ANSRBR",
+      "common_name": "Black Duck",
+      "scientific_name": "Anas rubripes",
+      "land_preference": [13,14,15,16,17,18,19]
+   },
+   "7": {
+      "name": "ANSSTR",
+      "common_name": "Gadwall",
+      "scientific_name": "Anas strepera",
+      "land_preference": [13,14,15,16,17,18,0,-1]
+   },
+   "8": {
+      "name": "ARDHRD",
+      "common_name": "Great Blue Heron",
+      "scientific_name": "Ardea herodias",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "9": {
+      "name": "ARNINT",
+      "common_name": "Ruddy Trunstone",
+      "scientific_name": "Arenaria interpres",
+      "land_preference": [13,14,15,19]
+   },
+   "10": {
+      "name": "AX1SPN",
+      "common_name": "Wood Duck",
+      "scientific_name": "Aix sponsa",
+      "land_preference": [0,13,14,15,16,17,18]
+   },
+   "11": {
+      "name": "BBLIBS",
+      "common_name": "Cattle Egret",
+      "scientific_name": "Bubulcus ibis",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "12": {
+      "name": "BRNBRN",
+      "common_name": "Brant",
+      "scientific_name": "Branta bernicla",
+      "land_preference": [0,-1]
+   },
+   "13": {
+      "name": "BRNCND",
+      "common_name": "Canada Goose",
+      "scientific_name": "Branta canadensis",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+   },
+   "14": {
+      "name": "BTRSTR",
+      "common_name": "Green-Backed Heron",
+      "scientific_name": "Butorides striatus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "15": {
+      "name": "CHLNGR",
+      "common_name": "Black Tern",
+      "scientific_name": "Chlidonias niger",
+      "land_preference": [19]
+   },
+   "16": {
+      "name": "CHRMLD",
+      "common_name": "Piping Plover",
+      "scientific_name": "Charadrius melodus",
+      "land_preference": [19]
+   },
+   "17": {
+      "name": "CHRSMP",
+      "common_name": "Semipalmated Plover",
+      "scientific_name": "Charadrius semipalmatus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,19]
+   },
+   "18": {
+      "name": "CHRVCF",
+      "common_name": "Killdeer",
+      "scientific_name": "Charadrius vociferus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,19]
+   },
+   "19": {
+      "name": "CHRWLS",
+      "common_name": "Wilson's Plover",
+      "scientific_name": "Charadrius wilsonia",
+      "land_preference": [19]
+   },
+   "20": {
+      "name": "CHTPLG",
+      "common_name": "Chimney Swift",
+      "scientific_name": "Chaetura pelagica",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12]
+   },
+   "21": {
+      "name": "CLDALB",
+      "common_name": "Sanderling",
+      "scientific_name": "Calidris alba",
+      "land_preference": [19]
+   },
+   "22": {
+      "name": "CLDALP",
+      "common_name": "Dunlin",
+      "scientific_name": "Calidris alpina",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,19]
+   },
+   "23": {
+      "name": "CLDCNT",
+      "common_name": "Red Knot",
+      "scientific_name": "Calidris canutus",
+      "land_preference": [19]
+   },
+   "24": {
+      "name": "CLDFSC",
+      "common_name": "White-Rumped Sandpiper",
+      "scientific_name": "Calidris fuscicollis",
+      "land_preference": [13,14,15]
+   },
+   "25": {
+      "name": "CLDMLN",
+      "common_name": "Pectoral Sandpiper",
+      "scientific_name": "Calidris melanotos",
+      "land_preference": [16,17,18]
+   },
+   "26": {
+      "name": "CLDMNT",
+      "common_name": "Least Sandpiper",
+      "scientific_name": "Calidris minutilla",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+   },
+   "27": {
+      "name": "CLDMR1",
+      "common_name": "Western Sandpiper",
+      "scientific_name": "Calidris mauri",
+      "land_preference": [13,14,15,19]
+   },
+   "28": {
+      "name": "CLDPSL",
+      "common_name": "Semipalmated Sandpiper",
+      "scientific_name": "Calidris pusilla",
+      "land_preference": [13,14,15,19]
+   },
+   "29": {
+      "name": "CRCCYN",
+      "common_name": "Northern Harrier",
+      "scientific_name": "Circus cyaneus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+   },
+   "30": {
+      "name": "CRGATR",
+      "common_name": "Black Vulture",
+      "scientific_name": "Coragups atratus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12]
+   },
+   "31": {
+      "name": "CRVOSS",
+      "common_name": "Fish Crow",
+      "scientific_name": "Corvus ossifragus",
+      "land_preference": [16,17,18,19]
+   },
+   "32": {
+      "name": "CSMALB",
+      "common_name": "Great Egret",
+      "scientific_name": "Gasmerodius albus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "33": {
+      "name": "CSTPLS",
+      "common_name": "Marsh Wren",
+      "scientific_name": "Cistothorus palustris",
+      "land_preference": [13,14,15]
+   },
+   "34": {
+      "name": "CTHAR1",
+      "common_name": "Turkey Vulture",
+      "scientific_name": "Cathartes aura",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12]
+   },
+   "35": {
+      "name": "CTPSMP",
+      "common_name": "Willet",
+      "scientific_name": "Catoptrophorus semipalmatus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,19]
+   },
+   "36": {
+      "name": "DNDDSC",
+      "common_name": "Prairie Warbler",
+      "scientific_name": "Dendroica discolor",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12]
+   },
+   "37": {
+      "name": "DNDPTC",
+      "common_name": "Yellow Warbler",
+      "scientific_name": "Dendroica petechia",
+      "land_preference": [16,17,18]
+   },
+   "38": {
+      "name": "EGRCRL",
+      "common_name": "Little Blue Heron",
+      "scientific_name": "Egretta caerulea",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "39": {
+      "name": "EGRTHL",
+      "common_name": "Snowy Egret",
+      "scientific_name": "Egretta thula",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "40": {
+      "name": "EGRTRC",
+      "common_name": "Tricolored Heron",
+      "scientific_name": "Egretta tricolor",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "41": {
+      "name": "ERMALP",
+      "common_name": "Horned Lark",
+      "scientific_name": "Eremophila aplestris",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12]
+   },
+   "42": {
+      "name": "FLCCLM",
+      "common_name": "Merlin",
+      "scientific_name": "Falco columbarius",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12]
+   },
+   "43": {
+      "name": "FLCPRG",
+      "common_name": "Peregrine Falcon",
+      "scientific_name": "Falco peregrinus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,19]
+   },
+   "44": {
+      "name": "GTHTRC",
+      "common_name": "Common Yellowthroat",
+      "scientific_name": "Geothlypis trichas",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,16,17,18]
+   },
+   "45": {
+      "name": "GV1IMM",
+      "common_name": "Common Loon",
+      "scientific_name": "Gavia immer",
+      "land_preference": [0,-1]
+   },
+   "46": {
+      "name": "HMTPLL",
+      "common_name": "American Oystercatcher",
+      "scientific_name": "Haematopus palliatus",
+      "land_preference": [13,14,15,19]
+   },
+   "47": {
+      "name": "HRNRST",
+      "common_name": "Barn Swallow",
+      "scientific_name": "Hirundo rustica",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,16,17,18]
+   },
+   "48": {
+      "name": "IXBEXL",
+      "common_name": "Least Bittern",
+      "scientific_name": "Ixobrychus exilis",
+      "land_preference": [13,14,15,16,17,18]
+   },
+   "49": {
+      "name": "LMNGRS",
+      "common_name": "Short-Billed Dowitcher",
+      "scientific_name": "Limnodromus griseus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+   },
+   "50": {
+      "name": "LMSFD1",
+      "common_name": "Marbled Godwit",
+      "scientific_name": "Limosa fedoa",
+      "land_preference": [13,14,15]
+   },
+   "51": {
+      "name": "LRSARG",
+      "common_name": "Herring Gull",
+      "scientific_name": "Larus argentatus",
+      "land_preference": [0,-1,2,3,4,5,6,7,8,9,10,11,12,19]
+   },
+   "52": {
+      "name": "LRSATR",
+      "common_name": "Laughing Gull",
+      "scientific_name": "Larus atricilla",
+      "land_preference": [0,-1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "53": {
+      "name": "LRSDLW",
+      "common_name": "Ring-Billed Gull",
+      "scientific_name": "Larus delawarensis",
+      "land_preference": [0,-1,2,3,4,5,6,7,8,9,10,11,12,19]
+   },
+   "54": {
+      "name": "LRSMRN",
+      "common_name": "Great Black-Backed Gull",
+      "scientific_name": "Larus marinus",
+      "land_preference": [0,-1,2,3,4,5,6,7,8,9,10,11,12,19]
+   },
+   "55": {
+      "name": "MLNNGR",
+      "common_name": "Black Scoter",
+      "scientific_name": "Melanitta nigra",
+      "land_preference": [0,-1]
+   },
+   "56": {
+      "name": "MLNPRS",
+      "common_name": "Surf Scoter",
+      "scientific_name": "Melanitta perspicillata",
+      "land_preference": [0,-1]
+   },
+   "57": {
+      "name": "MLSMLD",
+      "common_name": "Song Sparrow",
+      "scientific_name": "Melospiza melodia",
+      "land_preference": [16,17,18]
+   },
+   "58": {
+      "name": "MLTATR",
+      "common_name": "Brown-Headed Cowbird",
+      "scientific_name": "Molothrus ater",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12]
+   },
+   "59": {
+      "name": "MRGSRR",
+      "common_name": "Red-Breasted Merganser",
+      "scientific_name": "Mergus serrator",
+      "land_preference": [-1]
+   },
+   "60": {
+      "name": "NMNPHP",
+      "common_name": "Whimbrel",
+      "scientific_name": "Numernius phaeopus",
+      "land_preference": [13,14,15,19]
+   },
+   "61": {
+      "name": "NYCNYC",
+      "common_name": "Black-Crowned Night Heron",
+      "scientific_name": "Nycticorax nycticorax",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "62": {
+      "name": "NYCVLC",
+      "common_name": "Yellow-Crowned Night Heron",
+      "scientific_name": "Nycticorax violaceus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "63": {
+      "name": "OCNOCN",
+      "common_name": "Wilson's Storm Petrel",
+      "scientific_name": "Oceanites oceanicus",
+      "land_preference": [-1]
+   },
+   "64": {
+      "name": "PDLPDC",
+      "common_name": "Pied-Billed Grebe",
+      "scientific_name": "Podilymbus podiceps",
+      "land_preference": [13,14,15,16,17,18]
+   },
+   "65": {
+      "name": "PFFGRS",
+      "common_name": "Sooty Shearwater",
+      "scientific_name": "Puffinus griseus",
+      "land_preference": [-1]
+   },
+   "66": {
+      "name": "PHSCLS",
+      "common_name": "Ring-Necked Pheasant",
+      "scientific_name": "Phasianus colchicus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12]
+   },
+   "67": {
+      "name": "PLCERY",
+      "common_name": "American White Pelican",
+      "scientific_name": "Pelecanus erythrorhynchos",
+      "land_preference": [0,-1]
+   },
+   "68": {
+      "name": "PLCOCC",
+      "common_name": "Brown Pelican",
+      "scientific_name": "Pelecanus occidentalis",
+      "land_preference": [19]
+   },
+   "69": {
+      "name": "PLGPLC",
+      "common_name": "Glossy Ibis",
+      "scientific_name": "Plegadis falcinellus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
+   },
+   "70": {
+      "name": "PLVSQT",
+      "common_name": "Black-Bellied Plover",
+      "scientific_name": "Pluvialis squatarola",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,19]
+   },
+   "71": {
+      "name": "PNDHLT",
+      "common_name": "Osprey",
+      "scientific_name": "Pandion haliaetus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,0,-1]
+   },
+   "72": {
+      "name": "PRGSBS",
+      "common_name": "Purple Martin",
+      "scientific_name": "Progne subis",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+   },
+   "73": {
+      "name": "QSCMJR",
+      "common_name": "Boat-Tailed Grackle",
+      "scientific_name": "Quiscalus major",
+      "land_preference": [13,14,15,19]
+   },
+   "74": {
+      "name": "RLLLNG",
+      "common_name": "Clapper Rail",
+      "scientific_name": "Rallus longirostris",
+      "land_preference": [13,14,15]
+   },
+   "75": {
+      "name": "RPRRPR",
+      "common_name": "Bank Swallow",
+      "scientific_name": "Riparia riparia",
+      "land_preference": [-1,0,2,3,4,5,6,7,8,9,10,11,12,15,18]
+   },
+   "76": {
+      "name": "RYNNGR",
+      "common_name": "Black Skimmer",
+      "scientific_name": "Rynchops niger",
+      "land_preference": [13,14,15,19]
+   },
+   "77": {
+      "name": "SL1BSS",
+      "common_name": "Northern Gannet",
+      "scientific_name": "Sula bassanus",
+      "land_preference": [-1]
+   },
+   "78": {
+      "name": "STLSRR",
+      "common_name": "Northern Rough-Winged Swallow",
+      "scientific_name": "Stelgidopteryx serripennis",
+      "land_preference": [-1,0,2,3,4,5,6,7,8,9,10,11,12,15,18]
+   },
+   "79": {
+      "name": "STRANT",
+      "common_name": "Least Tern",
+      "scientific_name": "Sterna antillarum",
+      "land_preference": [0,-1,13,14,15,19]
+   },
+   "80": {
+      "name": "STRCSP",
+      "common_name": "Caspian Tern",
+      "scientific_name": "Sterna caspia",
+      "land_preference": [0,-1,13,14,15,19]
+   },
+   "81": {
+      "name": "STRFRS",
+      "common_name": "Forster's Tern",
+      "scientific_name": "Sterna forsteri",
+      "land_preference": [0,-1,13,14,15,19]
+   },
+   "82": {
+      "name": "STRHRN",
+      "common_name": "Common Tern",
+      "scientific_name": "Sterna hirundo",
+      "land_preference": [0,-1,13,14,15,19]
+   },
+   "83": {
+      "name": "STRMGN",
+      "common_name": "Eastern Meadowlark",
+      "scientific_name": "Sturnella magna",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,16,17,18]
+   },
+   "84": {
+      "name": "STRMXM",
+      "common_name": "Royal Tern",
+      "scientific_name": "Sterna maxima",
+      "land_preference": [0,-1,13,14,15,19]
+   },
+   "85": {
+      "name": "STRNLT",
+      "common_name": "Gull-Billed Tern",
+      "scientific_name": "Sterna nilotica",
+      "land_preference": [0,-1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,19]
+   },
+   "86": {
+      "name": "STRSND",
+      "common_name": "Sandwich Tern",
+      "scientific_name": "Sterna sandvicensis",
+      "land_preference": [0,-1,13,14,15,19]
+   },
+   "87": {
+      "name": "STRVLG",
+      "common_name": "European Starling",
+      "scientific_name": "Sturnus vulgaris",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12]
+   },
+   "88": {
+      "name": "TCHBCL",
+      "common_name": "Tree Swallow",
+      "scientific_name": "Tachycineta bicolor",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+   },
+   "89": {
+      "name": "THRLDV",
+      "common_name": "Carolina Wren",
+      "scientific_name": "Thryothorus ludovicianus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12]
+   },
+   "90": {
+      "name": "TRNFLN",
+      "common_name": "Lesser Yellowlegs",
+      "scientific_name": "Tringa flavipes",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+   },
+   "91": {
+      "name": "TRNMLN",
+      "common_name": "Greater Yellowlegs",
+      "scientific_name": "Tringa melanoleuca",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+   },
+   "92": {
+      "name": "TRNSLT",
+      "common_name": "Solitary Sandpiper",
+      "scientific_name": "Tringa solitaria",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+   },
+   "93": {
+      "name": "TYRTYR",
+      "common_name": "Eastern Kingbird",
+      "scientific_name": "Tyrannus tyrannus",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12,16,17,18]
+   },
+   "94": {
+      "name": "ZNDMCR",
+      "common_name": "Mourning Dove",
+      "scientific_name": "Zenaida macroura",
+      "land_preference": [2,3,4,5,6,7,8,9,10,11,12]
+   }
+}
